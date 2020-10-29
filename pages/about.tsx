19310131bc68d4daa8959a7e9c6457911128df57
sato <sato_m@mediaxis.jp>
2020-10-29 14:55:35 +0900

    Add Redirect

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,16 +1,32 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import Image from "next/image";
+import { useEffect } from "react";
+import Layout from "../components/Layout";
 
-const AboutPage = () => (
-  <Layout title="About | Next.js + TypeScript Example">
-    <h1>About</h1>
-    <p>This is the about page</p>
-    <p>
-      <Link href="/">
-        <a>Go home</a>
-      </Link>
-    </p>
-  </Layout>
-)
+const AboutPage: NextPage = () => {
+  const router = useRouter();
+  useEffect(() => {
+    const query = router.query;
+    console.log(query);
+    if (query.foo) {
+      router.push("/");
+    }
+  }, [router]);
 
-export default AboutPage
+  return (
+    <Layout title="About | Next.js + TypeScript Example">
+      <h1>About</h1>
+      <Image src="/images/nextjs.png" width="633" height="516" />
+      <p>This is the about page</p>
+      <p>
+        <Link href="/">
+          <a>Go home</a>
+        </Link>
+      </p>
+    </Layout>
+  );
+};
+
+export default AboutPage;
